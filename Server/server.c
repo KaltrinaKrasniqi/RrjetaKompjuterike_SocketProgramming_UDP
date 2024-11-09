eacdee68823b3f02655a6ed0fd943f5769f00082
KaltrinaKrasniqi <116883212+KaltrinaKrasniqi@users.noreply.github.com>
2024-11-09 17:58:57 +0100

    krijimi i metodes per me shtu permbajtje ne file dhe per me i logu veprimet e klientit me te dhena

--- Server/server.c
+++ Server/server.c
@@ -13,3 +13,32 @@
 #define PORT 8888
 #define TIMEOUT 10 
 #define WAITING_TIMEOUT 10 
+
+
+void appendFile(const char* filename, const char* content) {
+    FILE* file = fopen(filename, "a"); 
+    if (file != NULL) {
+        fputs(content, file);
+        fclose(file);
+    }
+    else {
+        printf("Error opening log file!\n");
+    }
+}
+
+
+void logRequest(const char* client_ip, const char* request) {
+    time_t current_time;
+    char* time_str;
+    time(&current_time);
+    time_str = ctime(&current_time);
+
+    // Removing newline from ctime
+    time_str[strlen(time_str) - 1] = '\0';
+
+    char log_entry[BUFLEN];
+    snprintf(log_entry, sizeof(log_entry), "[%s] Request from %s: %s\n", time_str, client_ip, request);
+
+    // Append the log entry to the log file
+    appendFile("server_log.txt", log_entry);
+}
